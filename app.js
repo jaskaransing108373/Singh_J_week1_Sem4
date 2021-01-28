f671eed385b0158aabd617727ff89548b8b3a4e2
jaskaransing108373 <jaskaransing108373@github.com>
2021-01-28 05:34:26 -0500

    add

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const http = require('http'); // like a php require
+const http = require('http'); // like a PHP require
 
-// require is more or less the same s a JS import
-
+// require is more or less the same as a JS import
 // const hostname = '127.0.0.1'; // this is localhost
-const port = process.env.PORT || 3000; //localhost:3000
+const port = process.env.PORT || 3000; // localhost:3000
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('sup world');
+    res.end('1 day and counting til the orange idiot is gone!');
 });
 
 server.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server is now running at ${port}/`);
 });
