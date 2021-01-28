c48cd32b4002811fb27d6787300d8c47045fa3fa
jaskaransing108373 <jaskaransing108373@github.com>
2021-01-28 08:45:19 -0500

    adding express

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const http = require('http'); // like a php require
+const express = require('express'); // like a php require
+const path = require('path');
 
-// require is more or less the same s a JS import
+const server = express();
 
-// const hostname = '127.0.0.1'; // this is localhost
+//set our views directory
+server.set("views", path.join(__dirname, 'views'));
+// set the statis assetss director so express knows where to look
+// for thr files, JS files, images et - anything static
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get("/", (req, res) => {
+    console.log('you have hit the home route');
+    res.sendFile('views/index.html');
+})
+
+server.get("/contact", (req, res) => {
+    console.log('hit the contact route');
+    res.sendFile('views/contact.html');
+})
+
 const port = process.env.PORT || 3000; //localhost:3000
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('sup world');
-});
 
 server.listen(port, () => {
     console.log(`Server is now running at ${port}/`);
